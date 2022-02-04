8aced18f5badfc3d62629ad2c8722cbe4ae96e0d
xphos <lquinn@vt.edu>
2022-02-04 18:37:15 -0500

    works for threads = N

--- module3/assignment/assignment.cu
+++ module3/assignment/assignment.cu
@@ -1,12 +1,12 @@
 //Based on the work of Andrew Krepps
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 __global__
 void gpu_add(int* a, int* b, int*c)
 {
     const int tid = (blockIdx.x * blockDim.x) + threadIdx.x;
-    printf("tid: val is %d\n", (a[tid]+b[tid]) );
     c[tid] = a[tid] + b[tid];
 }
 
@@ -39,69 +39,82 @@ void gen_data(int* a) {
 
 void print_result(int* arr, int N, char opt){
 
-    int test_idx = 5;// rand() % N;
+    int test_idx = rand() % N;
     printf("%c operation result c[%d]=%d\n", opt, test_idx, arr[test_idx]);
 }
 
 int main(int argc, char** argv)
 {
-	// read command line arguments
-	int totalThreads = 256;//(1 << 20);
-	int blockSize = 256;
-	
-	if (argc >= 2) {
-		totalThreads = atoi(argv[1]);
-	}
-	if (argc >= 3) {
-		blockSize = atoi(argv[2]);
-	}
+    // read command line arguments
 
-	int numBlocks = totalThreads/blockSize;
+    int N = 1 << 20; // allocate 2^20 = 1024*1024 ~ 1 mil
+
+    int totalThreads = (1 << 20);
+    srand(time(0));
+    int blockSize = 256;
+    
+    if (argc >= 2) {
+        totalThreads = atoi(argv[1]);
+    }
+    if (argc >= 3) {
+        blockSize = atoi(argv[2]);
+    }
+    
+    if (argc >= 4) {
+        N = atoi(argv[3]);
+    }
+
+
+    int numBlocks = totalThreads/blockSize;
     int threadPerBlock = totalThreads / numBlocks;
 
 
-	// validate command line arguments
-	if (totalThreads % blockSize != 0) {
-		++numBlocks;
-		totalThreads = numBlocks*blockSize;
-		
-		printf("Warning: Total thread count is not evenly divisible by the block size\n");
-		printf("The total number of threads will be rounded up to %d\n", totalThreads);
-	}
+    // validate command line arguments
+    if (totalThreads % blockSize != 0) {
+        ++numBlocks;
+        totalThreads = numBlocks*blockSize;
+        
+        printf("Warning: Total thread count is not evenly divisible by the block size\n");
+        printf("The total number of threads will be rounded up to %d\n", totalThreads);
+    }
 
-    int N = totalThreads; //= 1 << 20; // allocate 2^20 = 1024*1024 ~ 1 mil
-
+   
     printf("numBlocks %d whith %d threads\n", numBlocks, threadPerBlock);
 
-    int* c = (int*)malloc(N);
+    // allocate data
+    int* c = (int*)malloc(N*sizeof(int));
     int* dev_a, *dev_b, *dev_c;
-
     cudaMalloc((void**)&dev_a, sizeof(int)* N);
     cudaMalloc((void**)&dev_b, sizeof(int)* N);
     cudaMalloc((void**)&dev_c, sizeof(int)* N);
 
-    cudaMemcpy(dev_a, c, N, cudaMemcpyHostToDevice);
-    cudaMemcpy(dev_b, c, N, cudaMemcpyHostToDevice);
-    cudaMemcpy(dev_c, c, N, cudaMemcpyHostToDevice);
+    // init data
+    //for(int i = 0; i < N; i++){
+    //    c[i] = 0;                      
+    //}
+    
+    cudaMemcpy(dev_a, c, N*sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_b, c, N*sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_c, c, N*sizeof(int), cudaMemcpyHostToDevice);
 
     // generate inputs on GPU because they are going to be used there
     gen_data<<<numBlocks, threadPerBlock>>>(dev_a);
     gen_data<<<numBlocks, threadPerBlock>>>(dev_b);
    
     gpu_add<<<numBlocks, threadPerBlock>>>(dev_a,dev_b,dev_c);
-    cudaMemcpy(c, dev_c, N*sizeof(long), cudaMemcpyDeviceToHost);
+    cudaMemcpy(c, dev_c, N*sizeof(int), cudaMemcpyDeviceToHost);    
     print_result(c, N, '+'); 
 
     gpu_sub<<<numBlocks, threadPerBlock>>>(dev_a,dev_b,dev_c);
-    cudaMemcpy(c, dev_c, N*sizeof(long), cudaMemcpyDeviceToHost);
+    cudaMemcpy(c, dev_c, N*sizeof(int), cudaMemcpyDeviceToHost);
     print_result(c, N, '-'); 
 
     gpu_mult<<<numBlocks, threadPerBlock>>>(dev_a,dev_b,dev_c);
-    cudaMemcpy(c, dev_c, N*sizeof(long), cudaMemcpyDeviceToHost);
+    cudaMemcpy(c, dev_c, N*sizeof(int), cudaMemcpyDeviceToHost);
     print_result(c, N, '*'); 
     
     gpu_mod<<<numBlocks, threadPerBlock>>>(dev_a,dev_b,dev_c);
-    cudaMemcpy(c, dev_c, N*sizeof(long), cudaMemcpyDeviceToHost);
+    cudaMemcpy(c, dev_c, N*sizeof(int), cudaMemcpyDeviceToHost);
     print_result(c, N, '%'); 
     
     cudaFree(dev_a);
@@ -110,8 +123,4 @@ int main(int argc, char** argv)
     free(c);
 
 }
-
-
-
-
 
