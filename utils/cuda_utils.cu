e1156b69805c18fa988c8dadcd0740a58bfb3ada
xphos <lquinn@vt.edu>
2022-03-18 22:41:12 -0400

    attempt 1

--- utils/cuda_utils.cu
+++ utils/cuda_utils.cu
@@ -1,12 +1,25 @@
 #include "cuda_utils.cuh"
+#include "cufft.h"
 
-cudaError_t checkCuda(cudaError_t result)
+
+
+cudaError_t checkCuda_z(cudaError_t result, char* file, int line)
 {
   if (result != cudaSuccess) {
-    fprintf(stderr, "CUDA Runtime Error: %s\n",
-            cudaGetErrorString(result));
+    fprintf(stderr, "CUDA Runtime Error: %s\n in %s:%d",
+            cudaGetErrorString(result), file, line);
     assert(result == cudaSuccess);
   }
   return result;
 }
 
+
+cufftResult_t checkCufft_z(cufftResult_t result, char* file, int line)
+{
+  if (result != CUFFT_SUCCESS) {
+    fprintf(stderr, "cufft Runtime Error: \n in %s:%d",
+            file, line);
+    assert(result == cudaSuccess);
+  }
+  return result;
+}
--- utils/cuda_utils.cuh
+++ utils/cuda_utils.cuh
@@ -2,15 +2,20 @@
 
 #include <stdio.h>
 #include <assert.h>
+#include "cufft.h"
 
 typedef unsigned int uint32_t;
 typedef unsigned char uint8_t;
 
-cudaError_t checkCuda(cudaError_t result);
+#define checkCuda(ans) {checkCuda_z(ans, __FILE__, __LINE__);}
+cudaError_t checkCuda_z(cudaError_t result, const char* file, int line);
+
+#define checkCufft(ans) {checkCufft_z(ans, __FILE__, __LINE__);}
+cufftResult_t checkCufft_z(cufftResult_t result, const char* file, int line);
 
 #define checkCudaKernel(x) do { \
         (x); \
-        checkCuda( cudaPeekAtLastError() );\
+        checkCuda_z( cudaPeekAtLastError(), __FILE__,__LINE__);\
     } while(0)
 
 
